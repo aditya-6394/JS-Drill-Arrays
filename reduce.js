28a3c451e8f5ca519fd93752f5b8209685d9a2c3
Aditya Singh <aditya6394@outlook.com>
2023-09-02 13:57:56 +0530

    Made implementation of reduce function generic

--- reduce.js
+++ reduce.js
@@ -1,18 +1,37 @@
-// const elements = [1, 2, 3, 4, 5, 5];
+// // const elements = [1, 2, 3, 4, 5, 5];
 
-function cb(sum,element){
-    return sum+element
+// function cb(sum,element){
+//     return sum+element
+// }
+
+// function reduce(elements,cb,startingValue){    
+//     let sum = 0;
+//     if(startingValue == undefined){
+//         startingValue =0;
+//     }
+//     for(let i=startingValue; i < elements.length; i++){        
+//         sum = cb(sum,elements[i]);
+//     }
+//     return sum;
+// }
+
+// module.exports = {reduce,cb};
+
+
+function reduce(array, callback, initialValue) {
+  if (array.length === 0 && initialValue === undefined) {
+    throw new TypeError('Reduce of empty array with no initial value');
+  }
+
+  let accumulator = initialValue !== undefined ? initialValue : array[0];
+  const startIndex = initialValue !== undefined ? 0 : 1;
+
+  for (let i = startIndex; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i], i, array);
+  }
+
+  return accumulator;
 }
 
-function reduce(elements,cb,startingValue){    
-    let sum = 0;
-    if(startingValue == undefined){
-        startingValue =0;
-    }
-    for(let i=startingValue; i < elements.length; i++){        
-        sum = cb(sum,elements[i]);
-    }
-    return sum;
-}
+module.exports = reduce;
 
-module.exports = {reduce,cb};
--- testReduce.js
+++ testReduce.js
@@ -1,5 +1,14 @@
-const obj = require('./reduce');
+// const obj = require('./reduce');
 
-const elements = [1, 2, 3, 4, 5, 5];
+// const elements = [1, 2, 3, 4, 5, 5];
 
-console.log(obj.reduce(elements,obj.cb,2));
+// console.log(obj.reduce(elements,obj.cb,2));
+
+const reduce = require('./reduce');
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = reduce(numbers, (accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum); 
